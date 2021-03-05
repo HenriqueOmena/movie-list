c68f5624c08b2257a9ba7eea301efb50deb65e9c
HenriqueOmena <88lmao@gmail.com>
2021-03-05 16:02:38 +0000

    Removed unecessary file

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,18 @@ import React from "react";
 import Routes from "routes";
 import { BrowserRouter } from "react-router-dom";
 import { Container } from "./App.styles";
-import Provider from "store/config/provider";
 import { ModalProvider } from "hooks/useModal";
 import { Modal } from "components/Modal";
 
 function App() {
   return (
     <BrowserRouter>
-      <Provider>
-        <ModalProvider>
-          <Container>
-            <Routes />
-            <Modal />
-          </Container>
-        </ModalProvider>
-      </Provider>
+      <ModalProvider>
+        <Container>
+          <Routes />
+          <Modal />
+        </Container>
+      </ModalProvider>
     </BrowserRouter>
   );
 }
